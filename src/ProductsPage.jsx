4642e4fdb8a6016778e687b5658810f233882fc8
maryhiguera <marylin0higuera@gmail.com>
2025-06-19 19:48:47 -0600

    Initial commit

---
+++ src/ProductsPage.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./Header"
+import { Footer } from "./Footer"
+
+
+export function ProductsPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
